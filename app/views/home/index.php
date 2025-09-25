59bf48f68c5886a08658f9073a2aa5aa3dc14b79
MontyBlack19 <reateguida678@gmail.com>
2025-09-25 02:23:51 -0500

    Cambios para mejorar el login la cita

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -6,7 +6,7 @@
 <body>
     <!-- Botón flotante para login de administradores/estilistas -->
 
-    <a href="/ProyectoFinalG4/app/views/login.php" class="admin-login-btn" title="Acceso administradores y estilistas">
+    <a href="index.php?controller=Login&action=login" class="admin-login-btn" title="Acceso administradores y estilistas">
         <i class="bi bi-person-circle" style="font-size: 1.5rem;"></i>
     </a>
 
--- app/views/review/editarAdmin.php
+++ app/views/review/editarAdmin.php
@@ -6,7 +6,7 @@
     <title>Editar Reseña (Admin)</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
-    <link href="/ProyectoFinalG4_USER/app/assets/css/review.css" rel="stylesheet">
+    <link href="/ProyectoFinalG4/app/assets/css/review.css" rel="stylesheet">
 </head>
 <body>
     <div class="container py-5">
